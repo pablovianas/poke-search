a823617eef736a001d7c9b31d007837cb4ea5790
Pablo Viana <96837989+pablovianas@users.noreply.github.com>
2022-05-09 12:33:20 -0300

    Updated few html, css and javascript lines

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,87 +1,83 @@
 const pokeInput = document.querySelector('#poke-search');
 const btnSearch = document.querySelector('#btn-search');
-const pokemon = document.querySelector('.card-pokemon');
+const cardPokemon = document.querySelector('.card-pokemon');
 
-pokeInput.addEventListener('keyup', showValueInput);
+pokeInput.addEventListener('keyup', sendInformationWithEnterKey);
 
+btnSearch.addEventListener('click', function () {
+    let value = document.querySelector('#poke-search').value;
+    getPokemonInformations(value);
+});
 
-function showValueInput(e){
-   if(e && e.keyCode === 13){
-        getPokemon(pokeInput.value);
-   }
+/**
+ * 
+ * 
+ */
 
+function sendInformationWithEnterKey(event){
+    if (event && event.keyCode === 13){
+       getPokemonInformations(pokeInput.value);
+    }
 }
-const getPokemon = (value) =>{
-    let html = '';
-    fetch(`https://pokeapi.co/api/v2/pokemon/${value}`).then((resposta) => {
 
-        return resposta.json();
-    }).then((resposta) => {
+const getPokemonInformations = (value) =>{
+    let html = '';
+    fetch(`https://pokeapi.co/api/v2/pokemon/${value.toLowerCase()}`).then((response) => {
+
+        return response.json();
+    }).then((response) => {
       
-        const r = resposta;
+        const pokemonInfo = response; 
    
             html += `
                 <div class="poke-name"> 
-                    <h3> #${r.id}</h3>
-                    <h3>${r.species.name}</h3>
+                    <h3> #${pokemonInfo.id}</h3>
+                    <h3>${pokemonInfo.species.name}</h3>
                 </div>
                 <ul class="pokemon-list">
 
                     <li>
                         <div>
-                            <img id="poke-photo" src="${r.sprites.other.dream_world.front_default}">
+                            <img id="poke-photo" src="${pokemonInfo.sprites.other.dream_world.front_default}">
                         </div>
                              
                     </li>
                 </ul>
 
-
                 <ul class="pokemon-list">
-
                     <li>
                         <div class="poke-type">
-                            <b> ${r.types[0].type.name} </b>
+                            <b> ${pokemonInfo.types[0].type.name} </b>
                         </div>
-
-
                     </li>
                 </ul>
 
                 <hr>
-                <h3> Abilities: </h3>
+                <h3> Abilities </h3>
             `
-        for (let i = 0; i < r.abilities.length; i++) {
+        for (let i = 0; i < pokemonInfo.abilities.length; i++) {
 
             html += `
-               
                 <ul>
-
                     <li>
-                            <div>
-                                <b class="abilities-name">${r.abilities[i].ability.name}</b>
-                            </div>
-        
+                        <div>
+                            <p class="abilities-name">${pokemonInfo.abilities[i].ability.name}</p>
+                        </div>
                     </li>
                 </ul>
             `
             
         }      
-        pokemon.innerHTML = html;
-        pokemon.style.display = 'block';
+        cardPokemon.innerHTML = html;
+        cardPokemon.classList.add('display');
+
     }).catch(function (error) {
         html += `
-            <h3> Error 404 - Pokemon not found! </h3>
+            <h4 style="text-align:center;"> Error 404 - Pokemon not found! </h4>
         `
-
-        pokemon.innerHTML = html;
-        pokemon.style.display = 'block';
+        cardPokemon.innerHTML = html;
+        cardPokemon.classList.add('display');
     });
     
    
 }
-
-btnSearch.addEventListener('click', function(){
-    let value = document.querySelector('#poke-search').value;
-    getPokemon(value);
-
-});
