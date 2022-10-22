70aea5ec7cb6bf933535b71d010ddaab4a8231f6
pablovianas <pabloviana2010@gmail.com>
2022-10-22 10:46:43 -0300

    Modified scripts js to be a module file

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,83 +1,22 @@
-const pokeInput = document.querySelector('#poke-search');
-const btnSearch = document.querySelector('#btn-search');
-const cardPokemon = document.querySelector('.card-pokemon');
+import { screen } from "./objects/render.js"
 
-pokeInput.addEventListener('keyup', sendInformationWithEnterKey);
+const form = document.querySelector('.pokemon-search')
 
-btnSearch.addEventListener('click', function () {
-    let value = document.querySelector('#poke-search').value;
-    getPokemonInformations(value);
-});
-
-/**
- * 
- * 
- */
-
-function sendInformationWithEnterKey(event){
-    if (event && event.keyCode === 13){
-       getPokemonInformations(pokeInput.value);
-    }
-}
-
-const getPokemonInformations = (value) =>{
-    let html = '';
-    fetch(`https://pokeapi.co/api/v2/pokemon/${value.toLowerCase()}`).then((response) => {
-
-        return response.json();
-    }).then((response) => {
-      
-        const pokemonInfo = response; 
-   
-            html += `
-                <div class="poke-name"> 
-                    <h3> #${pokemonInfo.id}</h3>
-                    <h3>${pokemonInfo.species.name}</h3>
-                </div>
-                <ul class="pokemon-list">
-
-                    <li>
-                        <div>
-                            <img id="poke-photo" src="${pokemonInfo.sprites.other.dream_world.front_default}">
-                        </div>
-                             
-                    </li>
-                </ul>
-
-                <ul class="pokemon-list">
-                    <li>
-                        <div class="poke-type">
-                            <b> ${pokemonInfo.types[0].type.name} </b>
-                        </div>
-                    </li>
-                </ul>
-
-                <hr>
-                <h3> Abilities </h3>
-            `
-        for (let i = 0; i < pokemonInfo.abilities.length; i++) {
-
-            html += `
-                <ul>
-                    <li>
-                        <div>
-                            <p class="abilities-name">${pokemonInfo.abilities[i].ability.name}</p>
-                        </div>
-                    </li>
-                </ul>
-            `
-            
-        }      
-        cardPokemon.innerHTML = html;
-        cardPokemon.classList.add('display');
-
-    }).catch(function (error) {
-        html += `
-            <h4 style="text-align:center;"> Error 404 - Pokemon not found! </h4>
-        `
-        cardPokemon.innerHTML = html;
-        cardPokemon.classList.add('display');
-    });
+form.addEventListener('submit', async (event)=> {
+    event.preventDefault()
+    const pokemonNotFound = 404 
+    const pokemonName = document.getElementById('poke-search').value
+    const pokemonInfo = await getPokemons(pokemonName)
     
-   
+    pokemonInfo === pokemonNotFound ? screen.renderPokemonNotFound() : screen.renderPokemon(pokemonInfo)  
+})
+
+async function getPokemons(pokemonName){
+    const pokemon = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName.toLowerCase()}`)
+    if(pokemon.status === 404){
+        return pokemon.status
+    }else{
+        const pokemonJson = await pokemon.json()
+        return await pokemonJson
+    }
 }
